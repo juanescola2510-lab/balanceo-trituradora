4c44882f91a048da9d0ebefecbdb581f52699708
juanescola2510-lab <juan.escola2510@gmail.com>
2026-03-13 15:27:17 -0500

    Update app_balanceo.py

--- app_balanceo.py
+++ app_balanceo.py
@@ -19,7 +19,7 @@ with col_logo2:
     except:
         st.info("ℹ️ Logo no encontrado.")
 
-st.markdown("<h1 style='text-align: center;'>⚖️ Balanceo: Trituradora 405CR01</h1>", unsafe_Error=True)
+st.markdown("<h1 style='text-align: center;'>⚖️ Balanceo: Trituradora 405CR01</h1>", unsafe_allow_html=True)
 st.markdown("<p style='text-align: center;'>Sistema: 0° Norte (Y+) | Sentido: Antihorario</p>", unsafe_allow_html=True)
 
 # --- FUNCIONES MATEMÁTICAS ---
